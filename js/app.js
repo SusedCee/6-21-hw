709d84c5ad77f3a44aa058761dbfe52babdd8dc4
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-21 16:33:19 -0600

    First commit

---
+++ js/app.js
@@ -0,0 +1,78 @@
+console.log("Page is working");
+
+//avoid var Use const or let!!
+
+
+// I 	VARIABLES AND DATATYPES
+
+
+//A. Q + A
+
+//1. How do we assign a value to a variable?
+//First you have to create an object then use curly brackets
+//to have a list of variables with their vales.
+//Example:
+//  const skateboard = {
+// 	wheels: 4,
+// 	gripColor: "Toy Machine", 
+// 	graphic: "Monster graphic",
+// }
+
+//2. How do we change the value of a variable?
+//By typing:
+//nameOfObject["nameOfVariable"] = 'newValue';
+//or
+//variable = newValue;
+
+//3. How do we assign an existing variable to a new variable?
+// We can do the above (#2) or we can get a value from another
+//variable.
+
+//4. Remind me, what are declare, assign, and define?
+//declare is creating a variable 
+//assign is giving values to variables 
+//define is putting a function where you want to use it
+
+//5. What is pseudocoding and why should you do it?
+//Pseudocoding is writing down in comments what you want your code to
+//do before you use it or if you want to reference later after
+//you have finished for reference.
+
+
+//6. What percentage of time should be spent thinking about how
+// you're going to solve a problem vs actually typing in code to
+// solve it?
+//I think most of the time should be solving the problem first 
+//, but when staring off everything might take a really long time.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

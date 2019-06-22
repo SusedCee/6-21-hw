5279b654ce0c568ffc3d40a6017b2567cd367de8
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-22 16:35:50 -0600

    Seventh commit

--- js/app.js
+++ js/app.js
@@ -164,8 +164,23 @@ console.log("Page is working");
 // 	console.log(i);	
 // 	}
 
+//B. GET EVEN
 
+//Print out the numbers that are within the range of 1 - 100.
+// for (i = 1; i <= 100; i++) {
+// 	console.log(i);
+// }
 
+//Adjust your code to add a message next to even numbers only
+// that says: "<-- is an even number".
+
+for (i = 1; i <= 100; i++) {
+	if(i%2 === 0){
+		console.log(`${i} is an even number`);
+	} else {
+		console.log(i);
+	}
+}
 
 
 

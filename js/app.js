8d687873d6ab5451a5484f47b6774af1ee39ce61
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-22 16:55:24 -0600

    Eighth commit

--- js/app.js
+++ js/app.js
@@ -146,42 +146,60 @@ console.log("Page is working");
 //1. Write a loop that will print out all the numbers from 0 
 //to 10, inclusive.
 
-// for (i = 0; i <=10; i++) {
+// for (let i = 0; i <=10; i++) {
 // 	console.log(i);
 // }
 
 //2. Write a loop that will print out all the numbers from 10
 // up to and including 400.
 
-// for (i = 10; i <= 400; i++) {
+// for (let i = 10; i <= 400; i++) {
 // 	console.log(i);
 // }
 
 //3. Write a loop that will print out every third number 
 //starting with 11 and going no higher than 4000.
 
-// for (i = 11; i <= 4000; i += 3) {
+// for (let i = 11; i <= 4000; i += 3) {
 // 	console.log(i);	
 // 	}
 
 //B. GET EVEN
 
-//Print out the numbers that are within the range of 1 - 100.
-// for (i = 1; i <= 100; i++) {
+//1. Print out the numbers that are within the range of 1 - 100.
+// for (let i = 1; i <= 100; i++) {
 // 	console.log(i);
 // }
 
-//Adjust your code to add a message next to even numbers only
+//2. Adjust your code to add a message next to even numbers only
 // that says: "<-- is an even number".
 
-for (i = 1; i <= 100; i++) {
-	if(i%2 === 0){
-		console.log(`${i} is an even number`);
-	} else {
-		console.log(i);
-	}
-}
+// for (let i = 1; i <= 100; i++) {
+// 	if(i%2 === 0){
+// 		console.log(`${i} is an even number`);
+// 	} else {
+// 		console.log(i);
+// 	}
+// }
 
+//C. GIVE ME FIVE
+
+//1. For the numbers 0 - 100, print out "I found a number. 
+//High five!" if the number is a multiple of five.
+// for (let i = 0; i <= 100; i++) {
+// 	if (i%5 === 0) {
+// 		console.log("I found a number. High five!");
+// 	}
+// }
+
+//2. Adjust your code to add a message next to even numbers only 
+//that says: "<-- is an even number".
+
+// for (let i = 0; i <= 100; i++) {
+// 	if (i%2 === 0) {
+// 		console.log(`${i} is an even number`);
+// 	}
+// }
 
 
 
